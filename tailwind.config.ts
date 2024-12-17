dd6fdcf05d37ff79a53fd309ef252bd5d8148b63
kasimugur <kasiugur7@gmail.com>
2024-12-17 10:29:47 +0300

    To do header page full #13

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,9 @@ export default {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+	
   theme: {
+		
   	extend: {
   		colors: {
   			background: 'var(--background)',
