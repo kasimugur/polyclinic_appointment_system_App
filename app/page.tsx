109aa27161eab6ef27a24702953141d797ed00ea
kasimugur <kasiugur7@gmail.com>
2024-12-18 20:30:03 +0300

    footer added #17

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
     <>
-<div className="container px-5 max-h-screen">
+<div className="container px-5 min-h-screen">
 
 <Hero />
 </div>
