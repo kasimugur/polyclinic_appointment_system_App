d010be5d2393df4de366bfaf09a027f9f721a0ce
kasimugur <kasiugur7@gmail.com>
2024-12-24 12:24:09 +0300

    Make an appointment form will be made #28

--- app/(routes)/appointmentTime/page.tsx
+++ app/(routes)/appointmentTime/page.tsx
@@ -1,23 +1,119 @@
 'use client'
 import { Button } from '@/components/ui/button'
-import { ArrowLeft, Star } from 'lucide-react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { ArrowLeft, ChevronRight, Star } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import React from 'react'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export default function AppointmentTime() {
+  const time = ['09:20', "09:30", "09:00", "09:45"]
+  const time2 = ['10:20', "10:30", "10:00", "10:45"]
+  
   const router = useRouter()
   return (
     <div className='py-3  m-4  rounded-xl p-2 min-h-72  bg-white w-full border-b'>
       <Button onClick={() => router.push('/appointments')} className='font-sans px-6 py-[7] text-lg' variant={'ghost'}><ArrowLeft className='mr-1' size={16} /> Randevu Al</Button>
-    <div className="p-3">
-    <div className="flex  items-center ">
-        <div className="flex gap-1">
-          <label className='text-sm  font-bold '> Hekim: </label> 
-          <label className='text-sm  font-sans '> ESMA NUR GÖDELEK </label> 
+      <div className="p-3">
+        <div className="flex  items-center ">
+          <div className="flex gap-1">
+            <label className='text-sm  font-bold '> Hekim: </label>
+            <label className='text-sm  font-sans '> ESMA NUR GÖDELEK </label>
+          </div>
+          <Button variant={'ghost'}><Star className='text-yellow-400' /></Button>
         </div>
-          <Button variant={'ghost'}><Star className='text-yellow-400'/></Button>
-        </div>
-    </div>
+
+        <Tabs defaultValue="13.12.2025" className="w-full">
+          <TabsList className='bg-white my-2 px-4 py-3    flex justify-start gap-4 ' >
+            <TabsTrigger className='tabsTrigerBtn ' value="13.12.2025">
+              12.12.2025 - çarşamba
+            </TabsTrigger>
+            <TabsTrigger className='tabsTrigerBtn ' value="12.12.2025">
+              13.12.2025 -perşembe
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="13.12.2025">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-1">
+                <AccordionTrigger className=' px-3 ' >09:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-2">
+                <AccordionTrigger className=' px-3 ' >09:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-3">
+                <AccordionTrigger className=' px-3 ' >09:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-4">
+                <AccordionTrigger className=' px-3 ' >09:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+
+            </Accordion>
+          </TabsContent>
+          <TabsContent value="12.12.2025">
+            <Accordion type="single" collapsible className="w-full">
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-1">
+                <AccordionTrigger className=' px-3 ' >10:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time2.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-2">
+                <AccordionTrigger className=' px-3 ' >10:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time2.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-3">
+                <AccordionTrigger className=' px-3 ' >10:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time2.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+              <AccordionItem className='bg-polycgray  border rounded-xl' value="item-4">
+                <AccordionTrigger className=' px-3 ' >10:00 </AccordionTrigger>
+                <AccordionContent className='bg-white border-t flex items-center text-center gap-3 w-full'>
+                  {time2.map(time => <Button
+                    className='bg-polycbtn hover:bg-polycbtnhover hover:text-white focus:bg-polycbtnhover m-1 px-4 text-white'
+                    variant={'ghost'}> {time} </Button>)}
+                </AccordionContent>
+              </AccordionItem>
+
+            </Accordion>
+          </TabsContent>
+        </Tabs>
+
+      </div>
 
     </div>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,16 +9,45 @@ export default {
   ],
 	
   theme: {
-		
   	extend: {
   		colors: {
   			background: 'var(--background)',
   			foreground: 'var(--foreground)'
   		},
+			backgroundColor:{
+				polycgray:'#f5f5f5',
+				polycbtn:'#1890FF',
+				polycbtnhover:'#188fffd8'
+			},
+			borderColor: {
+				polycblue:'#1890FF' 
+			},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
